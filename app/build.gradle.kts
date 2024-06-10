07056bb8cd32245ec796085a05549ee5ab9de83f
Bruno Marques <brunomarques1337@hotmail.com>
2024-06-10 13:39:24 -0300

    BUILD: Correção do build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
+    id ("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
 android {
