5d2341fb9f02852967fd71a8181818e29a863ee4
Caiiru <caiogome125@gmail.com>
2024-06-11 16:50:09 -0300

    FEAT: Select Theme by recycler view

--- app/src/main/java/com/example/myquiz/ThemeAdapter.kt
+++ app/src/main/java/com/example/myquiz/ThemeAdapter.kt
@@ -1,38 +1,50 @@
 package com.example.myquiz
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myquiz.databinding.ItemThemeBinding
 
-class ThemeAdapter(private val themes:List<Int> ):RecyclerView.Adapter<ThemeAdapter.ThemeViewHolder>() {
+class ThemeAdapter(private val themes:List<Int>, private val clickListener: OnThemeClickListener ):RecyclerView.Adapter<ThemeAdapter.ThemeViewHolder>() {
     interface OnThemeClickListener{
         fun onThemeClick(view:View, position: Int)
     }
-    inner class ThemeViewHolder(view:View):RecyclerView.ViewHolder(view){
-        
-        fun bind(){
+    inner class ThemeViewHolder(val binding:ItemThemeBinding):RecyclerView.ViewHolder(binding.root){
 
+        fun bind(theme:Int){
+            DicionarioTemas.GetThemeIcon(theme)?.let { binding.themeIcon.setImageResource(it) }
+            DicionarioTemas.GetThemeById(theme)?.let { binding.themeName.text = it }
+
+            binding.root.setOnClickListener{
+                clickListener.onThemeClick(it,adapterPosition)
+            }
         }
 
-
+        /*
         val themeIcon:ImageView = view.findViewById(R.id.theme_icon)
         val themeName:TextView = view.findViewById(R.id.theme_name)
+
+         */
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThemeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_theme, parent,false)
-        return ThemeViewHolder(view)
+        val binding = ItemThemeBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return ThemeViewHolder(binding)
     }
 
     override fun getItemCount(): Int = themes.size
 
     override fun onBindViewHolder(holder: ThemeViewHolder, position: Int) {
         val theme = themes[position]
-        DicionarioTemas.GetThemeIcon(theme)?.let { holder.themeIcon.setImageResource(it) }
-        holder.themeName.text = DicionarioTemas.GetThemeById(theme);
+        themes[position].apply {
+            holder.bind(this)
+        }
+        Log.d("Adapter", "bind:$position")
     }
 
 }
--- app/src/main/java/com/example/myquiz/activities/ThemeSelectorActivity.kt
+++ app/src/main/java/com/example/myquiz/activities/ThemeSelectorActivity.kt
@@ -3,6 +3,7 @@ package com.example.myquiz.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myquiz.DicionarioTemas
@@ -16,12 +17,19 @@ class ThemeSelectorActivity : AppCompatActivity() {
 
         val themes = DicionarioTemas.GetThemes()
 
+        var selectedTheme:String
+
         for(id in themes){
             DicionarioTemas.GetThemeById(id)?.let { Log.d("DEBUG", it) }
         }
 
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         recyclerView.layoutManager = GridLayoutManager(this,2)
-        recyclerView.adapter = ThemeAdapter(themes)
+        recyclerView.adapter = ThemeAdapter(themes, object : ThemeAdapter.OnThemeClickListener{
+            override fun onThemeClick(view: View, position: Int) {
+                selectedTheme = DicionarioTemas.GetThemeById(themes[position])!!
+                Log.d("DEBUG", "Selected theme: $selectedTheme")
+            }
+        })
     }
 }
