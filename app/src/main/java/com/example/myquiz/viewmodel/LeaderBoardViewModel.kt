00d5699437247c493015991871296de012654950
Bruno <brunomarques1337@hotmail.com>
2024-06-11 02:14:20 -0300

    FEAT: LeaderBoardViewModel

---
+++ app/src/main/java/com/example/myquiz/viewmodel/LeaderBoardViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.myquiz.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.myquiz.model.LeaderBordRepository
+import com.example.myquiz.model.Player
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LeaderBoardViewModel @Inject constructor(private val lbRepository: LeaderBordRepository)
+    : ViewModel() {
+        val boardLiveData = MutableLiveData<List<Player>>()
+    fun add(nome: String, pontos: Int) {
+        lbRepository.add(nome, pontos)
+        boardLiveData.value = lbRepository.players
+    }
+    fun refresh() {
+        boardLiveData.value = lbRepository.players
+    }
+}
