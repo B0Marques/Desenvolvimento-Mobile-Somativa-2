7238ade075300660d38b04f03faa3e6c323fccfe
Bruno <brunomarques1337@hotmail.com>
2024-06-11 16:51:04 -0300

    REAFCTOR: Lógica da API

--- app/src/main/java/com/example/myquiz/model/Question.kt
+++ app/src/main/java/com/example/myquiz/model/Question.kt
@@ -1,5 +1,6 @@
 package com.example.myquiz.model
 
+import android.text.Html
 import com.google.gson.annotations.SerializedName
 
 data class Question(
@@ -7,4 +8,15 @@ data class Question(
     @SerializedName("question") val pq: String,
     @SerializedName("correct_answer") val correta: String,
     @SerializedName("incorrect_answers") val falsas: List<String>,
-)
+    @SerializedName("category") val tema: String
+) {
+    fun decodeHtml(): Question {
+        return Question(
+            dif,
+            Html.fromHtml(pq).toString(),
+            Html.fromHtml(correta).toString(),
+            falsas.map { Html.fromHtml(it).toString() },
+            Html.fromHtml(tema).toString()
+        )
+    }
+}
