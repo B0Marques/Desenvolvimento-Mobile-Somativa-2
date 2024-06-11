86c6ea4a272682c8624b590a6a29b13c83de3954
Caiiru <caiogome125@gmail.com>
2024-06-11 18:28:02 -0300

    WIP -> Question Activity

--- app/src/main/java/com/example/myquiz/activities/MainActivity.kt
+++ app/src/main/java/com/example/myquiz/activities/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mainMenu = Intent(this, MainMenuActivity::class.java);
+        var mainMenu = Intent(this, QuestionActivity::class.java);
         startActivity(mainMenu)
     }
 }
