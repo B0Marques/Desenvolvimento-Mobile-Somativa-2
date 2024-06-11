5c0cb8cc0a254a7f1a0a337f229221ee4ff684d9
Bruno <brunomarques1337@hotmail.com>
2024-06-11 14:48:53 -0300

    WIP -> Dicionário de Temas

---
+++ app/src/main/java/com/example/myquiz/DicionarioTemas.kt
@@ -0,0 +1,34 @@
+package com.example.myquiz
+
+class DicionarioTemas {
+    companion object {
+        private val map: Map<Int, String> = mapOf(
+            9 to "General Knowledge",
+            10 to "Entertainment: Books",
+            11 to "Entertainment: Film",
+            12 to "Entertainment: Music",
+            13 to "",
+            14 to "",
+            16 to "",
+            17 to "",
+            18 to "",
+            19 to "",
+            20 to "",
+            21 to "",
+            22 to "",
+            23 to "",
+            24 to "",
+            25 to "",
+            26 to "",
+            27 to "",
+            28 to "",
+            29 to "",
+            30 to "",
+            31 to "",
+            32 to ""
+
+            )
+    }
+
+
+}
